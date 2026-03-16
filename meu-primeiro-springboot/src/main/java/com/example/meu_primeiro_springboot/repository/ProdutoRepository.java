c95774d660024104a7ac19d717563466b7224f40
SamCodex01 <txtstring@gmail.com>
2026-03-15 21:40:27 -0300

    primeiro commit

---
+++ meu-primeiro-springboot/src/main/java/com/example/meu_primeiro_springboot/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.example.meu_primeiro_springboot.repository;
+
+import com.example.meu_primeiro_springboot.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
---
+++ meu-primeiro-springboot/src/main/java/com/example/meu_primeiro_springboot/service/ProdutoService.java
@@ -0,0 +1,33 @@
+package com.example.meu_primeiro_springboot.service;
+
+import com.example.meu_primeiro_springboot.model.Produto;
+import com.example.meu_primeiro_springboot.repository.ProdutoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository){
+
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> listarprodutos(){
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> buscarPorId(Long id){
+        return produtoRepository.findById(id);
+    }
+
+    public Produto salvarProduto(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public void deletarProduto(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+}
